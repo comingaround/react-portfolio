f10e8f308e4c0ead91943566c0a6c97934390333
comingaround <131857839+comingaround@users.noreply.github.com>
2024-05-23 16:13:25 +0300

    commit

--- src/components/home/CaseStudies.jsx
+++ src/components/home/CaseStudies.jsx
@@ -6,6 +6,29 @@ function CaseStudies() {
         <div className="case-studies">
             <div className="case-studies-preview">
                 <section>
+                    <img className='case_desk' src="/caseStudies/easypeasy.png" alt="1 " />
+                    <img className='case_tablet' src="/caseStudies/easypeasy_tablet.png" alt="1 " />
+                    <img className='case_phone' src="/caseStudies/easypeasy_phone.png" alt="1 " />
+                    <a href="https://easypeasy.studio/" target="_blank"></a>
+                </section>
+                <div className='case-studies-3'>
+                    <div>
+                        <div>
+                            <h1>Easy Peasy Studio</h1>
+                            <h1 style={{opacity: '0.6'}}>Stand Out! Get eaten!</h1>
+                        </div>
+                        <p style={{opacity: '0.6'}}>Year 2024</p>
+                    </div>
+                    <a href="https://easypeasy.studio/" target="_blank">
+                        easypeasy.studio
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 12 12" fill="none" style={{ marginLeft: '8px' }}>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.4299 0C11.5811 0 11.7261 0.0600593 11.833 0.166965C11.9399 0.273872 12 0.418867 12 0.570055V6.58155H10.8599V1.94489L0.804653 12.0037L0 11.199L10.0554 1.14011H5.78121V0H11.4299Z" fill="white"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div className="case-studies-preview">
+                <section>
                     <img className='case_desk' src="/caseStudies/charge.png" alt="1 " />
                     <img className='case_tablet' src="/caseStudies/charge_tablet.png" alt="1 " />
                     <img className='case_phone' src="/caseStudies/charge_phone.png" alt="1 " />
